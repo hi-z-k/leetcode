8f00588ec8b2dd288b52205d41291eb33e00c7cd
hi-z-k <104999321+hi-z-k@users.noreply.github.com>
2025-04-05 14:52:01 +0300

    Time: 49 ms (5.68%), Space: 22.7 MB (19.67%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,8 +1,3 @@
-// prepare two pointers at first initially pointing to index a= 0 and b=1
-//if the two elements getting pointed are the same, a doesn't change, indx(b) gets erased
-// if they are different, you make a point to b and b to the next element by incrementing
-// this code terminates when b reaches last pointer
-// return nums.length
 
 class Solution {
 public:
