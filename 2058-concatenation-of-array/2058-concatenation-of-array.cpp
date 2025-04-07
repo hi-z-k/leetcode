35680a857203063426bd3b4ff0dafc3b5cc8b96f
hi-z-k <104999321+hi-z-k@users.noreply.github.com>
2025-04-07 20:59:39 +0300

    Time: 1 ms (20%), Space: 17 MB (32.59%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.cpp
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -1,3 +1,7 @@
+//the code runs twice the number of the provided vector
+// add the elements to vector ans
+// modulo operator is implemented so that when i exceeds the index of num, it restarts the index from 0 
+
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
