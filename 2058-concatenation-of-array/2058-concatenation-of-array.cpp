356cb865b644395da2b9c5edf02ce7c34de6c7b2
hi-z-k <104999321+hi-z-k@users.noreply.github.com>
2025-04-07 15:36:12 +0300

    Time: 1 ms (20%), Space: 17.1 MB (17.87%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.cpp
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -3,11 +3,8 @@ public:
     vector<int> getConcatenation(vector<int>& nums) {
         vector<int> ans;
         int len = nums.size();
-        int idx{};
         for (int i = 0; i < 2*len; i++){
-            idx = i % len;
-            ans.push_back(nums.at(idx));
-            cout << ans.at(i) << endl;
+            ans.push_back(nums.at(i % len));
         }
         return ans;
     }
