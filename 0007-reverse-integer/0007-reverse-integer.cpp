348ff0c69c3e48aca5a7a5ec81cdf3a6d5c97eed
hi-z-k <104999321+hi-z-k@users.noreply.github.com>
2025-04-26 05:33:47 +0300

    Time: 4 ms (20.19%), Space: 9.2 MB (5.42%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,32 @@
+#import <vector>
+
+class Solution {
+public:
+    bool isInt(double x){
+        return !(x <= -2147483649 || x >= 2147483648);
+    }
+    int reverse(int x) {
+        if (!(isInt(x))) return 0;
+        bool isNeg = (x<0);
+        if(isNeg && !(isInt(-1*(double)x))) return 0;
+        x = (x>0)?x:-1*x;
+        vector<int> digits;
+        int power = 0;
+        while(true){
+            digits.push_back(x%10);
+            if (x%10==x) break;
+            x /= 10;
+            power++;
+        }
+        double reverse = 0;
+        for(int d: digits){
+            reverse += d*pow(10,(power));
+            if (!(isInt(reverse))) return 0;
+            cout << d << endl;
+            power--;
+        }
+        int rev = (int)reverse;
+        return (isNeg)?-1*rev:rev;
+    }
+};
+
