49cedff635404759deaa6b2e7f18dcf3fbc9109e
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2025-08-06 00:59:42 +0300

    Time: 11 ms (56.25%), Space: 17.9 MB (43.11%) - LeetHub

---
+++ 1044-find-common-characters/1044-find-common-characters.py
@@ -0,0 +1,33 @@
+class Solution:
+    def count(self,lst):
+        occurenece = {}
+        for data in lst:
+            occurenece[data] = occurenece.get(data,0) + 1
+        return occurenece
+
+    def compare(self,dict1, dict2):
+        keyDelete = []
+        for element in dict1.keys():
+            if dict2.get(element,0):
+                dict1[element] = min(dict1[element],dict2[element])
+            else:
+                keyDelete.append(element)
+        for elt in keyDelete:
+            del dict1[elt]
+        return dict1
+
+    def strOf(self,dict):
+        strOut = ""
+        for c in dict:
+            strOut += c*dict[c]
+        return list(strOut)
+
+
+    def commonChars(self, words: list[str]) -> list[str]:
+        if len(words)== 0: return []
+        superSet = self.count(words[0])
+        for word in words:
+            wordFreq = self.count(word)
+            superSet = self.compare(superSet,wordFreq)
+        return self.strOf(superSet)    
+        
