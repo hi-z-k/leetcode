f47a6224d807e13aa9e9fab6316b2f0149384a18
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2025-09-17 11:49:52 +0300

    Time: 83 ms (10.67%), Space: 28.9 MB (54.38%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
@@ -1,18 +1,18 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-        skill.sort()
-        i = 0
-        j = len(skill) - 1
-        sum = skill[i] + skill[j]
-        chemisty = 0
-        while(i<j):
-            if (skill[i] + skill[j] != sum): return -1
-            chemisty += skill[i] * skill[j]
-            i += 1
-            j -= 1
-        return chemisty
+        skillCounter = Counter(skill) 
+        numPairs = len(skill)//2
+        alreadyProcessed = set()
+        expectedSum = sum(skill)//numPairs
+        chemistry = 0
+        for num in skill:
+            if not skillCounter[num]:
+                continue
+            target = expectedSum - num 
+            if not skillCounter[target]:
+                return -1
+            skillCounter[target] -= 1
+            skillCounter[num] -= 1
+            chemistry += num * target 
+        return chemistry
 
-# skill => even length
-# taking pairs => pair sum is equal
-
-# [3,2,5,1,3,4] => [1,2,3,3,4,5]
