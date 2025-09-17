8d771b1cb147ffde8186d837aabe3c65bc562f78
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2025-09-17 18:09:35 +0300

    Time: 51 ms (33.54%), Space: 23.4 MB (20.98%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.py
+++ 0917-boats-to-save-people/0917-boats-to-save-people.py
@@ -4,8 +4,8 @@ class Solution:
         i, j = 0, len(people)-1
         numBoat = 0
         while(i<=j):
-            print(people[i],people[j],people[i] + people[j])
-            if people[i] + people[j] <= limit or i == j:
+            currentSum = people[i] + people[j] 
+            if currentSum <= limit or i == j:
                 i += 1
                 j -= 1
             elif people[j] <= limit:
