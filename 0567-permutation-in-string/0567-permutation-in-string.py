13c1b10e28f3a7d0e5d8cb2e8b1d4483de75839f
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2025-09-20 22:49:48 +0300

    Time: 49 ms (37.5%), Space: 17.9 MB (62.53%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,16 +1,17 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         window = len(s1)
-        checkStr = Counter(s1)
-        segmentStr = Counter(s2[:window])
+        targetCount = Counter(s1)
+        windowCount = Counter(s2[:window])
         i = 0
         j = window
 
         while (j <= len(s2)):
-            if checkStr == segmentStr:
+            if targetCount == windowCount:
                 return True
-            segmentStr[s2[i]] -= 1
-            if j < len(s2): segmentStr[s2[j]] = segmentStr.get(s2[j], 0) + 1
+            if j == len(s2): break
+            windowCount[s2[i]] -= 1
             i += 1
+            windowCount[s2[j]] += 1
             j += 1
         return False
