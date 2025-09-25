28a33376c7002c7210994906233f94311d393233
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2025-09-25 21:45:30 +0300

    Time: 15 ms (82.65%), Space: 20.1 MB (24.39%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.py
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -3,17 +3,15 @@ class Solution:
         numsHash = {}
         for index,num in enumerate(nums):
             if num not in numsHash:
-                val = {}
-                val["first"] = index
-                val["last"] = index
-                val["count"] = 1
-                numsHash[num] = val
+                numsHash[num] = {"first": index, "last": index, "count": 1}
             else:
                 numsHash[num]["count"] += 1
                 numsHash[num]["last"] = index
         return numsHash
+    def length(self,start, end):
+        return end - start + 1
     def findShortestSubArray(self, nums: List[int]) -> int:
         numsDict = self.numOccurence(nums)
-        maxFrequency = max([data["count"] for data in numsDict.values()])
-        possibleLength = [data["last"]-data["first"]+1 for data in numsDict.values() if data["count"]== maxFrequency]
-        return min(possibleLength)
+        maxFrequency = max((data["count"] for data in numsDict.values()))
+        leastLength = min((self.length(data["first"], data["last"]) for data in numsDict.values() if data["count"]== maxFrequency))
+        return leastLength
