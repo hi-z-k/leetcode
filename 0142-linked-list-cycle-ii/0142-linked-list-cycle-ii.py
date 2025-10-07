3d58ff930a4155993b0393f7fd5e88c435babbe5
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2025-10-07 22:04:50 +0300

    Time: 44 ms (72.51%), Space: 19.6 MB (80.18%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -1,16 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-# approach => first let's detect if there is a cycle to begin with => using FLoyd's Algorithm
-    # => having a fast pointer and a slow pointer, and the only chance they become equal is if there is a cycle
-    # => If we detect a cycle => reset slow pointer to the head and slow down the rabbit's speed from 2 to 1, when they meet will be
-    # the head of the cycle and also keep a count of this process
-    # time complexity => O(m+nC) where m is the non cyclic part and nC is a multiple of the cycle part => Boiling down to O(n)
-    # space complexity => O(1) we essentially need to maintain few variable that points to nodes
-
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         rabbit, turtle = head, head
