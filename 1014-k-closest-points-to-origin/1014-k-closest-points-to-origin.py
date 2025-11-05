52cc200b9227d566065871d3a546694dcf695d6c
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2025-11-05 23:46:32 +0300

    Time: 27 ms (97.98%), Space: 21.9 MB (97.45%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -5,17 +5,5 @@ class Solution:
         return ((x1)**2 + (y1)**2)
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         distance = Solution.squareDistanceToOrigin
-        lengthHash = {}
-        for point in points:
-            d = distance(point)
-            if d in lengthHash:
-                lengthHash[d].append(point)
-            else:
-                lengthHash[d] = [point]
-        dist = list(lengthHash.keys())
-        dist.sort()
-        output = []
-        length = min(k,len(dist))
-        for i in range(length):
-            output.extend(lengthHash[dist[i]])
-        return output[:k]
+        points.sort(key=distance)
+        return points[:k]
