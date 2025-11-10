15d43a4158c7354c83d93c4220ba9bf3729984d9
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2025-11-10 22:40:36 +0300

    Time: 7 ms (9.51%), Space: 18.7 MB (16.26%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.py
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.py
@@ -1,10 +1,6 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        #   you set the start when the ( stack and ) stack are empty
-        #   when the sizes of the two stack becomes equal(if not empty), record the locations in the hash
-        # go through the string once more and recreate the answer
         opening = []
-        closing = []
         start = 0
         marked = set()
         for i,p in enumerate(s):
