e0c1f406663f82f4df14f846561cfab7ebba8bb1
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2025-11-16 23:35:12 +0300

    Time: 0 ms (100%), Space: 17.8 MB (46.55%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.py
@@ -0,0 +1,21 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        freq = [0]*101
+        prefix = [0]*101
+        expected = [0]*len(heights)
+        for h in heights:
+            freq[h] += 1
+        for i in range(1,len(freq)):
+            prefix[i] = prefix[i-1] + freq[i]
+        freq = prefix
+        for i in range(len(heights)-1,-1,-1):
+            element = heights[i]
+            freqNum = freq[element]
+            if freqNum:
+                expected[freqNum-1] = element
+                freq[element] -= 1
+        count = 0
+        for i in range(len(expected)):
+            if expected[i] != heights[i]:
+                count += 1
+        return count
