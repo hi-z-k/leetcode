4288b2e90e369ffe976c0840768f3b6e6592aa46
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2025-11-19 23:17:39 +0300

    Time: 0 ms (100%), Space: 19.2 MB (10.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        requiredBefore = {}
-        for i, num in enumerate(nums):
-            requireNum = target-num
-            if requireNum in requiredBefore:
-                return [requiredBefore[requireNum], i]
-            requiredBefore[num] = i
+        targets = {}
+        for i,n in enumerate(nums):
+            needed = target - n
+            if n in targets:
+                return [targets[n],i]
+            targets[needed] = i
         return []
