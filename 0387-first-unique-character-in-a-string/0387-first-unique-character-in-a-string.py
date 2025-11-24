d879c9d2eff8cdf3e51be4b078184120682763e3
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2025-11-24 23:10:50 +0300

    Time: 51 ms (79.21%), Space: 18 MB (54.86%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,17 +1,8 @@
+from collections import Counter
 class Solution:
-    def count(self,lst):
-            occurrence = {}
-            for i, data in enumerate(lst):
-                if data not in occurrence:
-                    occurrence[data] = i
-                else:
-                    occurrence[data] = -1   
-            return occurrence
-
     def firstUniqChar(self, s: str) -> int:
-        freqCount = self.count(s)
-        for letter in freqCount:
-            index = freqCount[letter]
-            if index != -1:
-                return index
+        count = Counter(s)
+        for i,letter in enumerate(s):
+            if count[letter] == 1:
+                return i
         return -1
