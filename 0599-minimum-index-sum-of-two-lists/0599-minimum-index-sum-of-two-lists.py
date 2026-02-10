effc6d7f8f67b860f63c67e1da41c5d746e77a22
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2026-02-10 23:50:52 +0300

    Time: 12 ms (47.31%), Space: 20 MB (19.89%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -1,15 +1,21 @@
 class Solution:
+    @staticmethod
+    def hashmap(list1):
+        output = {}
+        for i,e in enumerate(list1):
+            output[e] = i
+        return output
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        idxList1 = {word: i for i,word in enumerate(list1)}
-
-        minVal = len(list1)+len(list2)
-        minList = []
-        for j,word in enumerate(list2):
-            if word in idxList1:
-                wordVal = j + idxList1[word]
-                if wordVal < minVal:
-                    minList = [word]
-                    minVal = wordVal
-                elif wordVal == minVal:
-                    minList.append(word)
-        return minList
+        hashmap = Solution.hashmap
+        map1 = hashmap(list1)
+        map2 = hashmap(list2)
+        common = set(list1) & set(list2)
+        commonMap = {}
+        for string in common:
+            commonMap[string] = map1[string] + map2[string]
+        minIdx = min(commonMap.values())
+        output = []
+        for string in commonMap:
+            if commonMap[string] == minIdx:
+                output.append(string)
+        return output
