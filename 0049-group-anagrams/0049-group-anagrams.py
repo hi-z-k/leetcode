d41add24db0bbb8daedc7c325fa0bb080a662255
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2026-02-11 14:43:51 +0300

    Time: 43 ms (6.23%), Space: 32.6 MB (8.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,7 +2,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagram_map = defaultdict(list)
         for word in strs:
-            count = hash(frozenset(Counter(word).items()))
+            count = frozenset(Counter(word).items())
             anagram_map[count].append(word)
         return list(anagram_map.values())
         
