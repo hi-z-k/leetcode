44869e8f7ba8da749ab4d304f4ef305225ed5fe7
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2026-02-12 20:19:42 +0300

    Time: 7 ms (90.75%), Space: 19.6 MB (16.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,3 @@
 class Solution:
-    def frequencyDict(self,lst):
-        occurenece = {}
-        for data in lst:
-            occurenece[data] = occurenece.get(data,0) + 1
-        return occurenece
-
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(t) != len(s): return False
-        
-        freqDictT = self.frequencyDict(t)
-        for char in s:
-            if freqDictT.get(char,0):
-                freqDictT[char] -= 1
-                continue
-            return False
-        return True
-            
+        return Counter(s) == Counter(t)
