3f3a048c8d641f36f4f736ce64863d65f11169bb
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2026-03-02 17:19:07 +0300

    Time: 115 ms (62.81%), Space: 34.4 MB (17.57%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,26 +1,19 @@
 class NumMatrix:
-    def prefixSum(self):
-        matrix = self.matrix
-        row  = self.row
-        column = self.column
-
+    @staticmethod
+    def prefix_sum(matrix):
+        row  = len(matrix)
+        column = len(matrix[0])
         prefix = [[0]*(column + 1) for i in range(row + 1)]
+
         for r, row in enumerate(matrix):
-            for e in range(len(row)):
-                prefix[r+1][e+1] = matrix[r][e] + prefix[r][e+1] + prefix[r+1][e] - prefix[r][e]
+            for c in range(len(row)):
+                prefix[r+1][c+1] = matrix[r][c] + prefix[r][c+1] + prefix[r+1][c] - prefix[r][c]
         return prefix
-    def print(self,matrix:List[List[int]]):
-        for row in matrix:
-            r = ""
-            for e in row:
-                r += f"{e} "
-            print(r)
+
     def __init__(self, matrix: List[List[int]]):
         self.matrix = matrix
-        self.row  = len(matrix)
-        self.column = len(matrix[0])
-        self.prefix = self.prefixSum()
+        self.prefix = NumMatrix.prefix_sum(matrix)
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        prefix = self.prefix
-        sum = prefix[row2+1][col2+1] - prefix[row1][col2+1] - prefix[row2+1][col1] + prefix[row1][col1]
-        return sum
+        p = self.prefix
+        sum_2d = p[row2+1][col2+1] - p[row1][col2+1] - p[row2+1][col1] + p[row1][col1]
+        return sum_2d
