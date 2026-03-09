d35d48fa1f49f642bc2b7598ee62031e353f886e
Hizkeal <104999321+hi-z-k@users.noreply.github.com>
2026-03-09 18:51:29 +0300

    Time: 0 ms (100%), Space: 19.4 MB (77.89%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -1,13 +1,12 @@
 class Solution:
-    def minOperations(self, logs: list[str]) -> int:
-        operations = 0
-        for action in logs:
-            if action == "../":
-                if operations:
-                    operations -= 1
-                continue
-            elif action == "./":
+    def minOperations(self, logs: List[str]) -> int:
+        level = 0
+        for directory in logs:
+            if directory == "../":
+                if level:
+                    level -= 1
+            elif directory == "./":
                 continue
             else:
-                operations += 1
-        return operations
+                level += 1
+        return level
